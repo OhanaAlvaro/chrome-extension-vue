4c2a5cc3455c500b9249acab0b068b2e23c803aa
asopenag <alex.sopena.91@gmail.com>
2020-09-10 15:43:19 +0200

    Initial Commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,18 +2,18 @@ module.exports = {
   root: true,
   env: {
     node: true,
-    webextensions: true,
+    webextensions: true
   },
   extends: [
-    "plugin:vue/essential",
-    "plugin:prettier/recommended",
-    "@vue/prettier",
+    'plugin:vue/essential',
+    'plugin:prettier/recommended',
+    '@vue/prettier'
   ],
   parserOptions: {
-    parser: "babel-eslint",
+    parser: 'babel-eslint'
   },
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-  },
-};
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+  }
+}
